33b3396f244d85006edda78f6817b35e60e200da
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2020-11-23 07:31:30 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Oscar Rodriguez Garrucho on 23/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PokedexSwifttUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwifttUIApp.swift
+//  Shared
+//
+//  Created by Oscar Rodriguez Garrucho on 23/11/20.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwifttUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
